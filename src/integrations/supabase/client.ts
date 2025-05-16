7a530b5e1f1557c9ed587db5a7df250b86f07bbd
MominMohammedImr <mominmohammedimran11@gmail.com>
2025-05-16 15:17:58 +0530

    changed supabase_url ,supabase_anonkey

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,10 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/lib/database.types';
 
 // Use absolute URLs here instead of environment variables
-const supabaseUrl = 'https://lbebqamzsrbeihzikmow.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZWJxYW16c3JiZWloemlrbW93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0NTA0ODcsImV4cCI6MjA1OTAyNjQ4N30.XMVCIfANC8Mo8Mf4BLHjeOmd-XTg5HTzynTwhp6c0sE';
+const supabaseUrl = 'https://cmpggiyuiattqjmddcac.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtcGdnaXl1aWF0dHFqbWRkY2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczNzkwNDksImV4cCI6MjA2Mjk1NTA0OX0.-8ae0vFjxM6FR8RgssFduVaBjfERURWQL8Wj3i5TujE';
 
 // Create a strongly typed Supabase client
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 export const useSupabaseClient = () => supabase;
+
