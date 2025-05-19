48a8ef0f56ad0fff2f55f185598453bbf89c1506
MominMohammedImr <mominmohammedimran11@gmail.com>
2025-05-19 13:03:02 +0530

    change in sitemap.xml.js

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,1 +1,28 @@
-v
+// pages/sitemap.xml.js
+
+export const getServerSideProps = async ({ res }) => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://b3f-prints.pages.dev/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  // ✅ Set critical headers to prevent Cloudflare/browser caching
+  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+
+  res.write(sitemap);
+  res.end();
+
+  return { props: {} };
+};
+
+export default function Sitemap() {
+  return null;
+}
