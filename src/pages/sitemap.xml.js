854ab7acc16558e89a8b119d0e984bb06581c418
MominMohammedImr <mominmohammedimran11@gmail.com>
2025-05-19 12:17:54 +0530

    add sitemap.xml.js

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,26 @@
+export async function getServerSideProps({ res }) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://yourdomain.com/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <!-- Add more URLs here -->
+</urlset>`;
+
+  // Set the Content-Type header to application/xml
+  res.setHeader('Content-Type', 'application/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function Sitemap() {
+  // This function will never be called because of getServerSideProps
+  return null;
+}
