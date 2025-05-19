d18c50ad706207f40421d8aec5e119f4f58bba91
MominMohammedImr <mominmohammedimran11@gmail.com>
2025-05-19 12:28:05 +0530

    changed sitemap.xml.js

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,16 +1,16 @@
+// src/pages/sitemap.xml.js
 export async function getServerSideProps({ res }) {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://yourdomain.com/</loc>
+    <loc>https://b3f-prints.pages.dev/</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>daily</changefreq>
+    <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
-  <!-- Add more URLs here -->
 </urlset>`;
 
-  // Set the Content-Type header to application/xml
+  // 👇 Set proper content type
   res.setHeader('Content-Type', 'application/xml');
   res.write(sitemap);
   res.end();
@@ -21,6 +21,5 @@ export async function getServerSideProps({ res }) {
 }
 
 export default function Sitemap() {
-  // This function will never be called because of getServerSideProps
-  return null;
+  return null; // This page doesn't render anything
 }
