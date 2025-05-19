52d9f82495d015b4b7bbb197440fafdc739c72b4
MominMohammedImr <mominmohammedimran11@gmail.com>
2025-05-19 13:31:13 +0530

    added new file

---
+++ src/pages/api/sitemap-index.js
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://b3f-prints.pages.dev/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+
+  res.status(200).send(sitemap);
+}
